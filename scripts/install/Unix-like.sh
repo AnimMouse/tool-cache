e7adc4254519b4d45dc4031ce12b8f9174dba10c
AnimMouse <git@animmouse.com>
2022-09-20 17:55:39 +0800

    Don't error when directory exists

--- scripts/install/Unix-like.sh
+++ scripts/install/Unix-like.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eu
 echo ::group::Installing $folder_name to tool cache
-if ! [ -d $RUNNER_TOOL_CACHE/$folder_name/ ]; then
-  mkdir $RUNNER_TOOL_CACHE/$folder_name/
-fi
+mkdir -p $RUNNER_TOOL_CACHE/$folder_name/
 mv $RUNNER_TEMP/$folder_name/* $RUNNER_TOOL_CACHE/$folder_name/
 echo ::endgroup::
