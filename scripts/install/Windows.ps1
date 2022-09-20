f05011576015394c62353bb9fdd319afdfd97d46
AnimMouse <git@animmouse.com>
2022-09-20 17:55:39 +0800

    Don't error when directory exists

--- scripts/install/Windows.ps1
+++ scripts/install/Windows.ps1
@@ -1,8 +1,6 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Installing $env:folder_name to tool cache
-if ( -not (Test-Path $env:RUNNER_TOOL_CACHE\$env:folder_name\ -PathType Container) ) {
-    New-Item $env:RUNNER_TOOL_CACHE\$env:folder_name\ -ItemType Directory
-}
+New-Item $env:RUNNER_TOOL_CACHE\$env:folder_name\ -ItemType Directory -Force
 Move-Item $env:RUNNER_TEMP\$env:folder_name\* $env:RUNNER_TOOL_CACHE\$env:folder_name\
 Write-Host ::endgroup::
