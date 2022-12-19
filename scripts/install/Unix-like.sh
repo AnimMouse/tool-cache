bc5964c3846fb3944cc9fad212068eba9b5f8915
AnimMouse <git@animmouse.com>
2022-12-19 19:45:22 +0800

    fix: Move folder itself than moving the files inside the folder

--- scripts/install/Unix-like.sh
+++ scripts/install/Unix-like.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -eu
 echo ::group::Installing $folder_name to tool cache
-mkdir -p $RUNNER_TOOL_CACHE/$folder_name/
-mv $RUNNER_TEMP/$folder_name/* $RUNNER_TOOL_CACHE/$folder_name/
+mv -T $RUNNER_TEMP/$folder_name $RUNNER_TOOL_CACHE/$folder_name
 echo ::endgroup::
