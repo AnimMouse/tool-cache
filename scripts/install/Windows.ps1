ffca1739ee922249b63b896c0f75546806bd7171
Anim Mouse <git@animmouse.com>
2023-10-28 01:21:57 +0800

    refactor: Improve test workflow reflecting real-world scenario

--- scripts/install/Windows.ps1
+++ scripts/install/Windows.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Installing $env:folder_name to tool cache
-New-Item $env:RUNNER_TOOL_CACHE\$env:folder_name\ -ItemType Directory -Force
+New-Item $env:RUNNER_TOOL_CACHE\$env:folder_name -ItemType Directory -Force
 Move-Item $env:RUNNER_TEMP\$env:folder_name\* $env:RUNNER_TOOL_CACHE\$env:folder_name -Force
 Write-Host ::endgroup::
